016ce0920c09c7ae6dcd3e4d6170ab7259ecec05
Sean <sean@malonetuning.com>
2022-03-21 11:52:54 -0700

    fix: only reproducible if .git dir is not available

--- reproduce.sh
+++ reproduce.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -x
 set -e
-docker run --rm -v $PWD:/app -w /app composer:2.1 composer require --dev phpunit/phpunit:^9
-docker run --rm -v $PWD:/app -w /app composer:2.2 composer require --dev phpunit/phpunit:^9
-
+docker run --rm -v $PWD/subdir:/app -w /app composer:2.1 composer require --dev phpunit/phpunit:^9 -vvv
+docker run --rm -v $PWD/subdir:/app -w /app composer:2.2 composer require --dev phpunit/phpunit:^9 -vvv
